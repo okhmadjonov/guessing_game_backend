005661de61eeb7eeb788c65ce8d60ac5e42bd755
okhmadjonov <oakhmadjonov1@gmail.com>
2023-12-21 13:10:57 +0500

    Fixed wrong attempts result show

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -46,6 +46,7 @@ namespace guessing_game_backend.Controllers
 
 
 
+
         [HttpPost("guess/{sessionId}")]
         public async Task<IActionResult> MakeGuess(string sessionId, string userInput)
         {
@@ -93,62 +94,68 @@ namespace guessing_game_backend.Controllers
 
                             return Ok("Congratulations! You've guessed the number.");
                         }
-                        else
+                    }
+                    else
+                    {
+                        session.Attempts--;
+                        game.Attempts++;
+                        int m = GameLogic.CalculateM(session.SecretNumber, userGuess);
+                        int p = GameLogic.CalculateP(session.SecretNumber, userGuess);
+                        m = m - p;
+
+
+                         var userIdClaim = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+                        if (int.TryParse(userIdClaim, out int uId))
                         {
-                            session.Attempts--;
-                            game.Attempts++;
-                            int m = GameLogic.CalculateM(session.SecretNumber, userGuess);
-                            int p = GameLogic.CalculateP(session.SecretNumber, userGuess);
-                            m = m - p;
-
-                            int id = await _gameRepository.CreateGame(game);
-                            // var userIdClaim = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-                            int uId;
-                            if (int.TryParse(userIdClaim, out uId))
-                            {
-                                if (userId != 0)
-                                {
-                                    var user = await _userRepository.GetUserById(userId);
-
-                                    if (user != null)
-                                    {
-                                        // Ensure the Games collection is initialized
-                                        if (user.Games == null)
-                                        {
-                                            user.Games = new List<Game>();
-                                        }
-
-                                        // Associate the game with the user
-                                        user.Games.Add(game);
-
-                                        await _userRepository.UpdateUser(user.Id, id);
-                                    }
-                                }
-                            }
-
                             if (session.Attempts == 0)
                             {
                                 session.Description.Add(userInput + " => " + $"Sorry, you didn't guess the number. The secret number was {string.Join("", session.SecretNumber)}.");
                                 _memoryCache.Remove(sessionId);
                                 game.Win = false;
                                 game.Description = session.Description;
+                                if (uId != 0)
+                               {
+                                var user = await _userRepository.GetUserById(uId);
+
+                                if (user != null)
+                                    {
+                                    // Ensure the Games collection is initialized
+                                    if (user.Games == null)
+                                    {
+                                        user.Games = new List<Game>();
+                                    }
+
+                                    // Associate the game with the user
+                                    user.Games.Add(game);
+                                    int id = await _gameRepository.CreateGame(game);
+                                    await _userRepository.UpdateUser(user.Id, id);
+                                  }
+                                 }
                                 return Ok($"Sorry, you didn't guess the number. The secret number was {string.Join("", session.SecretNumber)}.");
                             }
-
                             session.Description.Add(userInput + " => " + $"Incorrect guess. M: {m}, P: {p}. You have {session.Attempts} attempts left.");
                             return Ok($"Incorrect guess. M: {m}, P: {p}. You have {session.Attempts} attempts left.");
+
                         }
+
                     }
                 }
-
-                // Moved the "Invalid input" return statement here
-                return BadRequest("Invalid input. Please enter a 4-digit number.");
+                else
+                {
+                    return BadRequest("Invalid input. Please enter a 4-digit number.");
+                }
+            }
+            else
+            {
+                return NotFound("Invalid or expired session ID.");
             }
 
-            // Add a default return statement if none of the conditions are met
-            return NotFound("Invalid or expired session ID.");
+           return NotFound("Invalid or expired session ID.");
+
         }
+
 
     }
 }
